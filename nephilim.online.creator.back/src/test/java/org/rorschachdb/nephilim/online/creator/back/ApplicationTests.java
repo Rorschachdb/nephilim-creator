88da689ff30c393ea68ffba881775ac566ffd3a0
Gerald-Arawn <g.lecleuziat@gmail.com>
2022-11-03 08:29:20 +0100

    setting up project [NOC-3 NOC-1 NOC-11]

---
+++ nephilim.online.creator.back/src/test/java/org/rorschachdb/nephilim/online/creator/back/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rorschachdb.nephilim.online.creator.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
