15159fa65dac614ba2c724b4fd31d642b78e7465
rorshachdb <davbinder.public@gmail.com>
2022-11-07 14:34:00 +0100

    Adding Degree and Ka Element Enums (NOC-7)

---
+++ nephilim.online.creator.back/src/main/java/org/rorschachdb/nephilim/online/creator/back/model/enums/DegreeTypeEnum.java
@@ -0,0 +1,8 @@
+package org.rorschachdb.nephilim.online.creator.back.model.enums;
+
+/**
+ * Enumeration of Degree types
+ */
+public enum DegreeTypeEnum {
+    ARCANA_QUEST, ESOTERIC_QUEST, SIMULACRUM, ESOTERIC_KNOWLEDGE, OCCULT_ART;
+}
---
+++ nephilim.online.creator.back/src/main/java/org/rorschachdb/nephilim/online/creator/back/model/enums/KaElementEnum.java
@@ -0,0 +1,8 @@
+package org.rorschachdb.nephilim.online.creator.back.model.enums;
+
+/**
+ * Enumeration of Ka Elements
+ */
+public enum KaElementEnum {
+    FIRE, EARTH, WATER, MOON, AIR;
+}
