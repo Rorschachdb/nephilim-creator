6e020c9e0e24ef13a3d24d38c7ac3a809f8f83f3
rorshachdb <davbinder.public@gmail.com>
2022-11-15 21:05:46 +0100

    application skeleton (NOC-18)

---
+++ nephilim.online.creator.front/src/app/modules/admin-module/admin-module.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminMainComponent } from './components/admin-main/admin-main.component';
+
+
+
+@NgModule({
+  declarations: [
+    AdminMainComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModuleModule { }
